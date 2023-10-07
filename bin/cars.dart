964e5abafa8402d0e6ccdc0e6457a5a5d0cab678
Hoosite <malandin.danil@mail.ru>
2023-10-07 18:51:38 +0500

    final

--- bin/cars.dart
+++ bin/cars.dart
@@ -1,5 +1,22 @@
 import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  ;
+import 'models/getCars/getCars.dart';
+
+void main(List<String> arguments) async {
+  Dio httpClient = Dio();
+  String url = 'https://myfakeapi.com/api/cars/';
+  var response = await httpClient.get(url);
+  int minId = -1;
+  double minPrice = double.infinity;
+  GetCars getCars = GetCars.fromJson(response.data);
+
+  for (var e in getCars.cars) {
+    String newStr = e.price.substring(1);
+    double price = double.parse(newStr);
+    if (price < minPrice) {
+      minPrice = price;
+      minId = e.id;
+    }
+  }
+  print(minId);
 }
